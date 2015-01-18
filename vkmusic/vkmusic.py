5a67d6bfc87009a0ea85dfa568002bae9ae325f9
dimon <dimon@localhost.localdomain>
2015-01-18 17:09:06 +0100

    added documentation for functions

--- vkmusic/vkmusic.py
+++ vkmusic/vkmusic.py
@@ -6,18 +6,27 @@ import urllib
 from selenium import webdriver
 import json
 
-print 'VK music downloader by Dimon'
-
 
 def check_connection():
+    """
+    Function checks the availability of an Internet connection
+    :return: corresponding Boolean value
+    """
     try:
-        urllib2.urlopen('http://www.google.com', timeout = 1)
+        urllib2.urlopen('http://www.google.com', timeout=1)
         return True
-    except urllib2.URLError as err: pass
-    return False
+    except urllib2.URLError:
+        return False
 
 
 def get_access_attributes(login, password):
+    """
+    Function tries to get access data according VK user's data
+    :param login: VK login string
+    :param password: VK password string
+    :return: if there were no errors => dictionary with access attributes
+             otherwise => None
+    """
     try:
         driver = webdriver.Firefox()
         payload = {
@@ -28,32 +37,44 @@ def get_access_attributes(login, password):
             'response_type': 'token',
         }
         driver.get("https://oauth.vk.com/authorize?%s" % urllib.urlencode(payload))
-        driver.find_element_by_name("email").send_keys(vk_login)
-        driver.find_element_by_name("pass").send_keys(vk_password)
-        driver.find_element_by_id("install_allow").click()
+        driver.find_element_by_name('email').send_keys(vk_login)
+        driver.find_element_by_name('pass').send_keys(vk_password)
+        driver.find_element_by_id('install_allow').click()
         driver.close()
 
-        access_list = (driver.current_url.split("#"))[1].split("&")
-        return dict(access_token=(access_list[0].split("="))[1],
-                    expires_in=(access_list[1].split("="))[1],
-                    user_id=(access_list[2].split("="))[1])
+        access_list = (driver.current_url.split('#'))[1].split('&')
+        return {
+            'access_token': (access_list[0].split("="))[1],
+            'expires_in':   (access_list[1].split("="))[1],
+            'user_id':      (access_list[2].split("="))[1],
+        }
     except:
         driver.close()
         return None
 
 
 def get_audio_list(access_attr):
-    url = "https://api.vk.com/method/audio.get?" + \
-          "uid=" + access_attr['user_id'] + "&access_token=" + access_attr['access_token']
-    return (json.loads(urllib2.urlopen(url).read()))['response']
+    """
+    Functions gets an information about audio files
+    :param access_attr: dictionary with access attributes
+    :return: list of records according audio files
+    """
+    payload = {
+        'uid': access_attr['user_id'],
+        'access_token': access_attr['access_token']
+    }
+    url_request = "https://api.vk.com/method/audio.get?%s" % urllib.urlencode(payload)
+    json_response = urllib2.urlopen(url_request).read()
+    return (json.loads(json_response))['response']
 
-# main execution
+# --------- main execution ----------
+print 'VK music downloader by old-student'
 
 if not check_connection():
     print 'Not connected to Internet'
     exit()
 
-vk_login = raw_input('Please enter your VK login: ')
+vk_login = raw_input('Please enter your VK login: ').rstrip()
 vk_password = getpass.getpass('Please enter your VK password: ') if sys.stdin.isatty() \
     else raw_input('Please enter your VK password: ').rstrip()
 
